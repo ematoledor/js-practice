e6413fc0d8dc4c6697996e6a675fa2849a0355fd
Emanuel Toledo <ematolr@gmail.com>
2020-08-04 21:17:43 -0500

    finishd but need to fix some bugs, not recognizing generated meal

--- meal-maker.js
+++ meal-maker.js
@@ -1,44 +1,66 @@
 const menu = {
   _courses: {
-    appetizers [],
-    mains [],
-    desserts [],
+    appetizers: [],
+    mains: [],
+    desserts: [],
   },
   get appetizers() {
-
+    return this._courses.appetizers;
   },
-  set appetizers(appetizerIn) {
-
+  set appetizers(appetizers) {
+    this._courses.appetizers = appetizers;  
   },
   get mains() {
-
+    return this._courses.mains;
   },
-  set mains(mainsIn) {
-
+  set mains(mains) {
+    this._courses.mains = mains;
   },
     get desserts() {
-
+      return this._courses.desserts;
   },
-  set desserts(dessertsIn) {
-
+  set desserts(desserts) {
+    this._courses.desserts = desserts;
   },
-  const menu = {
-    _courses: {},
-    get courses() {
-      return {
-        appetizers: this.appetizers,
-        mains: this.mains,
-        desserts: this.desserts,
-      }
-    }
-    addDishToCourse(courseName, dishName, dishPrice,) {
-      const dish = {
-        name: dishName,
-        price: dishPrice,
-      };
-      return this._courses[courseName].push(dish);
-    }
-  }
+  get courses() {
+    return {
+      appetizers: this.appetizers,
+      mains: this.mains,
+      desserts: this.desserts,
+    };
+  },
+  addDishToCourse(courseName, dishName, dishPrice) {
+    const dish = {
+      name: dishName,
+      price: dishPrice,
+    };
+    return this._courses[courseName].push(dish);
+  },
+  getRandomDishFromCourse(courseName) {
+    const dishes = this._courses[courseName];
+    const randomIndex = Math.floor(Math.random() * dishes.length);
+    return dishes[randomIndex];
+  },
+  generateRandomMeal() {
+    const appetizers = this.getRandomDishFromCourse('appetizers');
+    const mains = this.getRandomDishFromCourse('mains');
+    const desserts = this.getRandomDishFromCourse('desserts');
+    const totalPrice = appetizer.price + main.price + dessert.price;
+    return `Your meal is ${appetizer.name}, ${main.name}, and ${dessert.name}, and total price is ${totalPrice}`;
+  } 
+};
 
+menu.addDishToCourse('appetizers', 'fries', 3.5);
+menu.addDishToCourse('appetizers', 'onion rings', 5.5);
+menu.addDishToCourse('appetizers', 'cheese sticks', 4.5);
 
-}
+menu.addDishToCourse('mains', 'garlic shrimp', 10.5);
+menu.addDishToCourse('mains', 'garlic fish', 14.5);
+menu.addDishToCourse('mains', 'garlic steak', 16.5);
+
+menu.addDishToCourse('desserts', 'ice cream', 5.5);
+menu.addDishToCourse('desserts', 'flan', 3.5);
+menu.addDishToCourse('desserts', 'cake', 2.5);
+
+const meal = meal.generateRandomMeal;
+console.log(meal);

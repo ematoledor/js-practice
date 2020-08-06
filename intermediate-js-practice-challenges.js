3c64e2607699b02e822bed0a25a29b4dc44d3d48
Emanuel Toledo <ematolr@gmail.com>
2020-08-06 16:01:14 -0500

    refactored convertToBaby function into an arrow function.

--- intermediate-js-practice-challenges.js
+++ intermediate-js-practice-challenges.js
@@ -53,7 +53,7 @@ for (let i = 0; i<arr.length; i++){
 
 // Write a function, convertToBaby(), that takes in an array as an argument and, using a loop, returns a new array with each string in the array prepended with 'baby '.
 
-function convertToBaby(arr) {
+const convertToBaby = arr => {
   let babyArray = [];
   for (let i = 0; i <= arr.length - 1; i++) {
     babyArray.push('baby ' + arr[i]);

616fd00faa177daedcb54d00432c17f3d0ea2987
Emanuel Toledo <ematolr@gmail.com>
2020-08-06 16:45:31 -0500

    fixed some code for nested loop, same item value was being used for both array iteration. i = firstArray, j = second array

--- intermediate-js-practice-challenges.js
+++ intermediate-js-practice-challenges.js
@@ -75,3 +75,29 @@ function convertToBaby(arr) {
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// We wrote a function, smallestPowerOfTwo(), which takes in an array.
+
+// Within our function, we create a new array results. We loop through the argument array and push in the smallest power of two which is greater than the current element.
+
+// It’s not doing what we want. Can you fix our code, please?
+
+const numbers = [5, 3, 9, 30];
+
+const smallestPowerOfTwo = arr => {
+  let results = [];
+  for (let i = 0; i < arr.length; i++) {
+    number = arr[i];
+    let j = 1;
+    while (j < number) {
+      j = j * 2;
+    }
+    results.push(j);
+  }
+  return results
+}
+
+console.log(smallestPowerOfTwo(numbers));
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////

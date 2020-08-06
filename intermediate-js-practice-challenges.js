de2d92a0b4b4618f74a7c153eba6e1d66fae72a0
Emanuel Toledo <ematolr@gmail.com>
2020-08-06 15:59:29 -0500

    function to convert animals to babies without map built-in method.

--- intermediate-js-practice-challenges.js
+++ intermediate-js-practice-challenges.js
@@ -49,3 +49,29 @@ for (let i = 0; i<arr.length; i++){
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Write a function, convertToBaby(), that takes in an array as an argument and, using a loop, returns a new array with each string in the array prepended with 'baby '.
+
+function convertToBaby(arr) {
+  let babyArray = [];
+  for (let i = 0; i <= arr.length - 1; i++) {
+    babyArray.push('baby ' + arr[i]);
+  }
+  return babyArray;
+}
+
+const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+
+console.log(convertToBaby(animals));
+
+// As a function declaration:
+function convertToBaby(arr) {
+  const babyArray = [];
+  for (let i = 0; i < arr.length; i++) {
+        babyArray.push('baby ' + arr[i]);
+  }
+  return babyArray
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////

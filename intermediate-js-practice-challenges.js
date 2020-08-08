a8b322a5d27582663df9a4b0469946ec1f1a4c16
Emanuel Toledo <ematolr@gmail.com>
2020-08-07 19:31:58 -0500

    function to sort years with sort built-in method.

--- intermediate-js-practice-challenges.js
+++ intermediate-js-practice-challenges.js
@@ -179,3 +179,18 @@ function shoutGreetings(arr) {
 const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo'];
 
 console.log(shoutGreetings(greetings))
+
+////////////////////////////////////////
+
+const sortYears = arr => arr.sort((x, y) => y - x);
+
+
+// As a function declaration AND using a named function:
+function sortYears(arr) {
+  const checkYears = (year1, year2) => year2 - year1
+  return arr.sort(checkYears)
+}
+
+const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
+
+console.log(sortYears(years))

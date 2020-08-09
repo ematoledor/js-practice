6a0d3070f48693b86835c92a2881f0c9d45c1cb5
Emanuel Toledo <ematolr@gmail.com>
2020-08-08 21:18:46 -0500

    alternate version for .every function

--- intermediate-js-practice-challenges.js
+++ intermediate-js-practice-challenges.js
@@ -223,6 +223,24 @@ console.log(justCoolStuff(myStuff, coolStuff))
 
 const isTheDinnerVegan = arr => arr.every(food => food.source === 'plant');
 
+///alternate
+function isTheDinnerVegan(arr) {
+  const isVegan = (food) => {
+        if (food.source === 'plant') {
+              return true;
+        }
+        return false;
+  }
+  for(let i = 0; i<arr.length; i++){
+        if (!isVegan(arr[i])){
+              return false 
+        }
+  }
+  return true
+}
+
 const dinner = [{name: 'hamburger', source: 'meat'}, {name: 'cheese', source: 'dairy'}, {name: 'ketchup', source:'plant'}, {name: 'bun', source: 'plant'}, {name: 'dessert twinkies', source:'unknown'}];
 
 console.log(isTheDinnerVegan(dinner))
+
+/////////////////////////

a44b445f74ce08715bac65bc4019d1401224bf79
Emanuel Toledo <ematolr@gmail.com>
2020-08-08 21:01:03 -0500

    function with filter built-in method

--- intermediate-js-practice-challenges.js
+++ intermediate-js-practice-challenges.js
@@ -194,3 +194,27 @@ function sortYears(arr) {
 const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
 
 console.log(sortYears(years))
+
+/////////////////////////////////////////////////////////
+
+// Write a function justCoolStuff() that takes in two arrays of strings, and, using the built-in .filter() method, returns an array with the items that are present in both arrays.
+
+const justCoolStuff = (firstArray, secondArray) => firstArray.filter(item => secondArray.includes(item))
+
+function justCoolStuff(firstArray, secondArray) {
+      function isInSecondArray(item){
+            for(let i = 0; i<secondArray.length; i++){
+                  if (secondArray[i] === item){
+                        return true
+                  }
+            }
+            return false 
+      }
+      return firstArray.filter(isInSecondArray)
+}
+
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+
+console.log(justCoolStuff(myStuff, coolStuff))

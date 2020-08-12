5321517e50eb6f9271a38bad484e8d78df92d343
Emanuel Toledo <ematolr@gmail.com>
2020-08-11 21:57:29 -0500

    same

--- intermediate-js-practice-challenges.js
+++ intermediate-js-practice-challenges.js
@@ -262,7 +262,7 @@ console.log(sortSpeciesByTeeth(speciesArray))
 
 const findMyKeys = arr => arr.findIndex(item => item === 'keys')
 
-/*
+
 // Alternate solution:
 // As a function declaration using a loop:
 function findMyKeys(arr) {
@@ -275,7 +275,6 @@ function findMyKeys(arr) {
       }
       return index
 }
-*/
 const randomStuff = ['credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'plastic spoon'];
 
 console.log(findMyKeys(randomStuff))  
